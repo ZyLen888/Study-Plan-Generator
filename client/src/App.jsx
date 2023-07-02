4c2eef547d2791edbbc3c82e6d311f3171d52329
Zhen Liu <MeimiLiu@users.noreply.github.com>
2023-07-02 22:57:01 +1000

    Created the frontend of the form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import styles from "./index.module.css";
+import planlogo from "./assets/planning.png";
 
 function App() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <img src={planlogo} alt="Plan logo" className={styles.icon} />
+      <h3>✨ Generate a Study Plan based on any topic given! ✨</h3>
+
+      <form>
+        <input
+          type="text"
+          name="plan-description"
+          placeholder="👀  Enter a topic of your interest!"
+        />
+        <input type="submit" value="🗓  Generate a plan!" />
+      </form>
+    </main>
+  );
 }
 
 export default App;
