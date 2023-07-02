5c53e18d58a450734acd96f1e15d6b4ddedc5340
Zhen Liu <MeimiLiu@users.noreply.github.com>
2023-07-02 22:59:14 +1000

    Handle user input using useState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import styles from "./index.module.css";
 import planlogo from "./assets/planning.png";
 
+import { useState } from "react";
+
 function App() {
+  const [planDesc, setPlanDesc] = useState("");
+
   return (
     <main className={styles.main}>
       <img src={planlogo} alt="Plan logo" className={styles.icon} />
@@ -12,6 +16,7 @@ function App() {
           type="text"
           name="plan-description"
           placeholder="👀  Enter a topic of your interest!"
+          onChange={(e) => setPlanDesc(e.target.value)}
         />
         <input type="submit" value="🗓  Generate a plan!" />
       </form>
